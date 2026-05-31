684aafbabd7363e8a74b698eddc0cb0244d43071
itismyspareacc001-eng <itismyspareacc001@gmail.com>
2026-05-31 17:04:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,6 @@ import google.generativeai as genai
 # GEMINI CONFIGURATION
 # ==========================================================
 
-import streamlit as st
-
 GEMINI_API_KEY = st.secrets[
     "GEMINI_API_KEY"
 ]
